3b0916518757abad7ef6927846cd2f7e1170a674
michimani <michimani210@gmail.com>
2024-12-15 01:26:04 +0900

    add docs

--- backend/get-started-open-feature/client/appconfig.py
+++ backend/get-started-open-feature/client/appconfig.py
@@ -20,12 +20,50 @@ from openfeature.provider import AbstractProvider, Metadata
 
 
 class Config:
+    """
+    A class used to represent the configuration for connecting to a service.
+
+    Attributes:
+        endpoint : str
+            The endpoint URL of the service.
+        credential : TokenCredential
+            The credential used for authentication.
+    Methods:
+        __init__(self, endpoint: str, credential: TokenCredential)
+            Initializes the Config object with the given endpoint and credential.
+    """
+
     def __init__(self, endpoint: str, credential: TokenCredential):
         self.endpoint = endpoint
         self.credential = credential
 
 
 class AppConfigurationProvider(AbstractProvider):
+    """
+    AppConfigurationProvider is a feature flag provider that integrates with Azure App Configuration.
+
+    Args:
+        config (Config): Configuration object containing endpoint and credential information.
+    Methods:
+        get_metadata() -> Metadata:
+            Returns metadata about the provider.
+        get_provider_hooks() -> List[Hook]:
+            Returns a list of hooks associated with the provider.
+        resolve_boolean_details(flag_key: str, default_value: bool, evaluation_context: EvaluationContext | None) -> FlagResolutionDetails[bool]:
+            Resolves the boolean flag value based on the provided flag key and evaluation context.
+        resolve_string_details(flag_key: str, default_value: str, evaluation_context: EvaluationContext | None) -> FlagResolutionDetails[str]:
+            Resolves the string flag value based on the provided flag key and evaluation context.
+        resolve_integer_details(flag_key: str, default_value: int, evaluation_context: EvaluationContext | None) -> FlagResolutionDetails[int]:
+            Resolves the integer flag value based on the provided flag key and evaluation context.
+        resolve_float_details(flag_key: str, default_value: float, evaluation_context: EvaluationContext | None) -> FlagResolutionDetails[float]:
+            Resolves the float flag value based on the provided flag key and evaluation context.
+        resolve_object_details(flag_key: str, default_value: Union[dict, list], evaluation_context: EvaluationContext | None) -> FlagResolutionDetails[Union[dict, list]]:
+            Resolves the object flag value based on the provided flag key and evaluation context.
+    Private Methods:
+        __resolve_value(flag_type: FlagType, flag_key: str, default_value: Any, evaluation_context: EvaluationContext | None = None) -> FlagResolutionDetails:
+            Internal method to resolve the flag value based on the flag type, flag key, and evaluation context.
+    """
+
     def __init__(self, config: Config):
         self.config = config
 
@@ -142,6 +180,17 @@ class AppConfigurationProvider(AbstractProvider):
 
 
 def init_provider() -> AppConfigurationProvider:
+    """
+    Initializes and returns an instance of AppConfigurationProvider.
+    This function creates a new AppConfigurationProvider using the endpoint
+    specified in the environment variable "APP_CONFIG_ENDPOINT" and the
+    DefaultAzureCredential for authentication.
+
+    Returns:
+        AppConfigurationProvider: An instance of AppConfigurationProvider configured
+        with the specified endpoint and credentials.
+    """
+
     return AppConfigurationProvider(
         Config(
             endpoint=environ.get("APP_CONFIG_ENDPOINT"),
